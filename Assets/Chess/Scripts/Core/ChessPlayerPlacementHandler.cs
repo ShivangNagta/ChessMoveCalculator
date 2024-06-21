4f24f85e89f79bf4c5e918d23dc1d2cd41e8a1b6
Shivang Nagta <141389245+ShivangNagta@users.noreply.github.com>
2024-06-22 04:37:08 +0530

    Added enemy pieces

--- Assets/Chess/Scripts/Core/ChessPlayerPlacementHandler.cs
+++ Assets/Chess/Scripts/Core/ChessPlayerPlacementHandler.cs
@@ -1,9 +1,18 @@
 ﻿using System;
+using Unity.Mathematics;
 using UnityEngine;
 
 namespace Chess.Scripts.Core {
+
     public class ChessPlayerPlacementHandler : MonoBehaviour {
+        public enum Team
+        {
+            Team1,
+            Team2
+        }
+
         [SerializeField] public int row, column;
+        [SerializeField] public Team teamOption;
 
         private void Start() {
             transform.position = ChessBoardPlacementHandler.Instance.GetTile(row, column).transform.position;
